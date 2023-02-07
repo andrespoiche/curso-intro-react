6b8f6d1af96e6e1f85ec260796f5576a96b0618d
andrespoiche <andrespoiche@gmail.com>
2023-02-07 18:41:14 -0400

    CSS en react

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch} from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+//import './App.css';
+const todos = [
+  {text: 'Corta cebolla', completed: true},
+  {text: 'Tomar el curso de intro a React', completed: false},
+  {text: 'Llorar con la llorona', completed: false},
+  {text: 'Lalala', completed:false},
+];
+
+function App() {
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch/>
+      <TodoList>
+      {todos.map(todo =>(
+        <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}/>
+      ))}
+      </TodoList>
+      <CreateTodoButton/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/CreateTodoButton.js
@@ -0,0 +1,10 @@
+import react from "react";
+import './CreateTodoButton.css'
+
+function CreateTodoButton(props) {
+    return(
+        <button className="CreateTodoButton">+</button>
+    );
+}
+
+export {CreateTodoButton};
