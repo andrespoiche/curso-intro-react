f1526999939672995041b8701781ef3912f8fa17
andrespoiche <andrespoiche@gmail.com>
2023-02-08 17:24:17 -0400

    Manejo de eventos

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,17 @@ import react from 'react';
 import './TodoSearch.css'
 
 function TodoSearch(){
+
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+    }
+
     return(
-        <input className='TodoSearch' placeholder="Cebolla"/>
+        <input 
+        className='TodoSearch' 
+        placeholder="Cebolla"
+        onChange={onSearchValueChange}
+        />
     );
 }
 
