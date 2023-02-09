63b734e69699edf73e01c66ec49745a487b9a175
andrespoiche <andrespoiche@gmail.com>
2023-02-09 12:04:40 -0400

    Contando y buscando TODOS

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,24 +1,21 @@
 import react from 'react';
 import './TodoSearch.css'
 
-function TodoSearch(){
-    const [searchValue, setSearchValue] = react.useState('');
-
+function TodoSearch({searchValue, setSearchValue}){
+  
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value);
     }
 
-    return[
+    return(
         <input 
         className='TodoSearch' 
         placeholder="Cebolla"
         value={searchValue}
         onChange={onSearchValueChange}
-        />,
-        <p>{searchValue}</p>
-
-    ];
+        />
+    );
 }
 
 export {TodoSearch};
