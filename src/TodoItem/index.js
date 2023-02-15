40b56bcaa31bbb5a5e70c3cf14ca2952edf3808c
andrespoiche <andrespoiche@gmail.com>
2023-02-15 17:40:34 -0400

    icon-component

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,29 +1,35 @@
 import React from "react";
 import './TodoItem.css'
+import { IconContext } from "react-icons";
+import { FiCheck, FiX } from "react-icons/fi";
 
 function TodoItem(props){
 
     
     return(
-        
+        <IconContext.Provider value={{
+            color: "lightblue",
+            size: "1em"
+        }
+        }>
         <li className="TodoItem">
             <span 
                 className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
                 onClick={props.onComplete}
                 >
-                 √
+            <FiCheck className=""/>
              </span>
-            <span></span>
-            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
                 {props.text}
             </p>
             <span 
                 className="Icon Icon-delete"
                 onClick={props.onDelete}
                 >
-                X
+            <FiX color="violet"/>
             </span>
         </li>
+        </IconContext.Provider>
     );
 }
 
